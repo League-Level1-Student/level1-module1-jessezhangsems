601f4b13ec21056f7a86c1a8216139cb8373d411
root <root@1a989c0756de>
2020-12-31 00:38:05 +0000

    ONLY POPCORN

--- src/_04_popcorn/popcornrunner.java
+++ src/_04_popcorn/popcornrunner.java
@@ -6,6 +6,10 @@ public class popcornrunner {
 public static void main(String[]args) {
 	String input=JOptionPane.showInputDialog("What flavor of popcorn do you want?");
 	Popcorn popcorn=new Popcorn(input);
+	String input1=JOptionPane.showInputDialog("How long do you want to heat it?");
+	int num=Integer.parseInt(input1);
+	
+	popcorn.applyHeat(num);
 	
 }
 }
